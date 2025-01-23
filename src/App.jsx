8f966dc15aef799c04590b5d04a5373a7c275afd
ahtisham02 <ahtisham02>
2025-01-23 13:05:45 +0500

    chnges

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,37 @@ import Error from "./Components/Pages/Error";
 import Profile from "./Components/Pages/Profile";
 import WatchList from "./Components/Pages/WatchList";
 import Intro from "./Components/Pages/Intro";
+import { useState } from "react";
 
 const App = () => {
   const Location = useLocation();
   const currentPath = Location.pathname;
+
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const [appliedFilters, setAppliedFilters] = useState({
+    language: "",
+    popularity: "",
+    releaseDate: ""
+  });
+
+  const applyFilters = (filters) => {
+    setAppliedFilters(filters);
+  };
+
   return (
     <div className="max-h-fit min-h-screen bg-black">
-      {currentPath === "/" ? null : <Header />}
+      {currentPath === "/" ? null : (
+        <Header filters={appliedFilters} onSearch={setSearchQuery} applyFilters={applyFilters} />
+      )}
       <Routes>
         <Route path="/" Component={Intro} />
         <Route path="/home" Component={Home} />
         <Route path="/movie/:id" Component={Movie} />
-        <Route path="/new" Component={Trending} />
+        <Route
+          path="/new"
+          element={<Trending filters={appliedFilters} searchQuery={searchQuery} />}
+        />
         <Route path="/watchlist" Component={WatchList} />
         <Route path="/profile" Component={Profile} />
         <Route path="/*" Component={Error} />
